630c3a9691809d88fce25da40856fec6296efc77
SyFeo <73487540+SyFeo@users.noreply.github.com>
2026-04-01 10:57:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 const CLIENT_ID = process.env.OSU_CLIENT_ID;
 const CLIENT_SECRET = process.env.OSU_CLIENT_SECRET;
